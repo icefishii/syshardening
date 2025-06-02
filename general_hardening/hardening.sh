59f7f791288caa09928f36058703da0d724fdb72
Benjamin Lampart <sera@icefish.dev>
2025-06-02 13:57:58 +0200

    removed non avalible packages

--- general_hardening/hardening.sh
+++ general_hardening/hardening.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "[+] Installing libpam-tmpdir apt-listbugs apt-listchanges"
-sudo apt install -y libpam-tmpdir apt-listbugs apt-listchanges
+sudo apt install -y libpam-tmpdir
 
 echo "[+] Copying sysctl hardening config"
 sudo cp "$SCRIPT_DIR/99-hardening.conf" /etc/sysctl.d/99-hardening.conf
